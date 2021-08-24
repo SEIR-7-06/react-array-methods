0a3535ec0f3ed67bcda297d8406fc74a6b0bd514
msolorio <msolorio.github@gmail.com>
2021-08-24 15:09:14 -0700

    Adds find method and notes for each array method

--- src/App.js
+++ src/App.js
@@ -15,18 +15,18 @@ class App extends React.Component {
   // }
 
   // Render a list of each of the cities name and population
-  // renderCities() {
-  //   const cityNames = cityData.map((city, idx) => {
-  //     return (
-  //       <li key={idx}>
-  //         <h2>{city.name}</h2>
-  //         <p>Population: {city.population}</p>
-  //       </li>
-  //     );
-  //   });
+  renderCities() {
+    const cityNames = cityData.map((city, idx) => {
+      return (
+        <li key={idx}>
+          <h2>{city.name}</h2>
+          <p>Population: {city.population}</p>
+        </li>
+      );
+    });
 
-  //   return cityNames;
-  // }
+    return cityNames;
+  }
 
   // Activity
   // Using the map method have renderCities return an array of JSX showing
@@ -51,18 +51,37 @@ class App extends React.Component {
   // Steps
   // 1. Get an array of the coastal towns
   // 2. Take the filtered array and convert it to an array of JSX
+  // renderCities() {
+  //   // Filtering through cityData to get an array of just the coastal towns
+  //   const coastalTowns = cityData.filter((city) => {
+  //     return city.isCoastal === true;
+  //   });
+
+  //   // Using the map method to convert the array into an array of JSX
+  //   const cityList = coastalTowns.map((city, idx) => {
+  //     return <li key={idx}>{city.name}</li>
+  //   });
+
+  //   return cityList;
+  // }
+
+  // Activity:
+  // Find the city with the name of Denver and render it to the page
+  // Render the name, state, and the population.
   renderCities() {
-    // Filtering through cityData to get an array of just the coastal towns
-    const coastalTowns = cityData.filter((city) => {
-      return city.isCoastal === true;
+    const denver = cityData.find((city) => {
+      return city.name === 'Denver';
     });
 
-    // Using the map method to convert the array into an array of JSX
-    const cityList = coastalTowns.map((city, idx) => {
-      return <li key={idx}>{city.name}</li>
-    });
+    const denverJSX = (
+      <div>
+        <h2>{denver.name}</h2>
+        <p>{denver.state}</p>
+        <p>{denver.population}</p>
+      </div>
+    )
 
-    return cityList;
+    return denverJSX;
   }
 
   // [<li>San Francisco</li>, <li>Phoenix</li>]
