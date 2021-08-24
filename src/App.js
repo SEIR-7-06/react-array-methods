af7ec698799d22e97929415b020864230f44c245
msolorio <msolorio.github@gmail.com>
2021-08-24 14:22:01 -0700

    Add looping examples into React app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
+import React from 'react';
+import cityData from './cityData';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  // Render a list of city names
+  // renderCities() {
+  //   const cityNames = cityData.map((city, idx) => {
+  //     return (
+  //       <h2 key={idx}>{city.name}</h2>
+  //     );
+  //   });
+
+  //   return cityNames;
+  // }
+
+  // Render a list of each of the cities name and population
+  // renderCities() {
+  //   const cityNames = cityData.map((city, idx) => {
+  //     return (
+  //       <li key={idx}>
+  //         <h2>{city.name}</h2>
+  //         <p>Population: {city.population}</p>
+  //       </li>
+  //     );
+  //   });
+
+  //   return cityNames;
+  // }
+
+  // Activity
+  // Using the map method have renderCities return an array of JSX showing
+  // - city name
+  // - state
+  // - is coastal or not
+  // renderCities() {
+  //   const citiesList = cityData.map((city) => {
+  //     return (
+  //       <li>
+  //         <h2>{city.name}</h2>
+  //         <p>{city.state}</p>
+  //         <p>{city.isCoastal ? 'Is a Coastal Town' : 'Is Inland'}</p>
+  //       </li>
+  //     )
+  //   })
+
+  //   return citiesList;
+  // }
+
+  // Show the coastal towns in the list
+  // Steps
+  // 1. Get an array of the coastal towns
+  // 2. Take the filtered array and convert it to an array of JSX
+  renderCities() {
+    // Filtering through cityData to get an array of just the coastal towns
+    const coastalTowns = cityData.filter((city) => {
+      return city.isCoastal === true;
+    });
+
+    // Using the map method to convert the array into an array of JSX
+    const cityList = coastalTowns.map((city, idx) => {
+      return <li key={idx}>{city.name}</li>
+    });
+
+    return cityList;
+  }
+
+  // [<li>San Francisco</li>, <li>Phoenix</li>]
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Looping in React</h1>
+        {this.renderCities()}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/cityData.js
@@ -0,0 +1,46 @@
+const cityData = [
+  {
+    name: 'San Francisco',
+    state: 'California',
+    population: 874961,
+    isCoastal: true
+  },
+  {
+    name: 'Phoenix',
+    state: 'Arizona',
+    population: 1633000,
+    isCoastal: false
+  },
+  {
+    name: 'Boston',
+    state: 'Massachusetts',
+    population: 675647,
+    isCoastal: true
+  },
+  {
+    name: 'New York',
+    state: 'New York',
+    population: 8804190,
+    isCoastal: true
+  },
+  {
+    name: 'Chicago',
+    state: 'Illinois',
+    population: 2746388,
+    isCoastal: false
+  },
+  {
+    name: 'Denver',
+    state: 'Colorado',
+    population: 715522,
+    isCoastal: false
+  },
+  {
+    name: 'San Diego',
+    state: 'California',
+    population: 1386932,
+    isCoastal: true
+  },
+]
+
+export default cityData;
